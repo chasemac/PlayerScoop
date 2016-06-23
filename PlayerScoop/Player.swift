6a70b24efa92f8431125b0333f6bc6c70e9453a7
Chase McElroy <ichasem@gmail.com>
2016-06-23 15:27:01 -0700

    Added players to Struct

--- PlayerScoop/Player.swift
+++ PlayerScoop/Player.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Player {
+struct Player {
     var name = ""
     var age = 0
     var college = ""
@@ -17,4 +17,19 @@ class Player {
     var backgroundColor = UIColor()
     var textColor1 = UIColor()
     var textColor2 = UIColor()
+    
+    static func createPlayers() -> [Player] {
+        
+        let por = Player(name: "Kristaps Porzingis", age: 20, college: "none", height: "7'3\"", image: UIImage(named: "por.png")!, backgroundColor: UIColor(red: 0.059, green: 0.208, blue: 0.471, alpha: 1.00), textColor1: UIColor.whiteColor(), textColor2: UIColor(red: 0.843, green: 0.161, blue: 0.008, alpha: 1.00))
+        
+        let dam = Player(name: "Damian Lillard", age: 25, college: "Weber State", height: "6' 3\"", image: UIImage(named: "dam.jpg")!, backgroundColor: UIColor.blackColor(), textColor1: UIColor.redColor(), textColor2: UIColor.whiteColor())
+        
+        let sheed = Player(name: "Rasheed Wallace", age: 41, college: "North Carolina", height: "6' 11\"", image: UIImage(named: "sheed.jpg")!, backgroundColor: UIColor(red: 0.106, green: 0.478, blue: 0.933, alpha: 1.00), textColor1: UIColor(red: 0.835, green: 0.165, blue: 0.176, alpha: 1.00), textColor2: UIColor.whiteColor())
+        
+        let jimbo = Player(name: "Jimmer Fredette", age: 26, college: "BYU", height: "6' 2\"", image: UIImage(named: "jimbo.jpg")!, backgroundColor: UIColor(red: 0.000, green: 0.161, blue: 0.322, alpha: 1.00), textColor1: UIColor.whiteColor(), textColor2: UIColor(red: 0.776, green: 0.655, blue: 0.478, alpha: 1.00))
+        
+        let adam = Player(name: "Adam Morrison", age: 31, college: "Gonzaga", height: "6' 8\"", image: UIImage(named: "adam.jpg")!, backgroundColor: UIColor(red: 0.149, green: 0.204, blue: 0.424, alpha: 1.00), textColor1: UIColor(red: 0.682, green: 0.090, blue: 0.125, alpha: 1.00), textColor2: UIColor.whiteColor())
+        
+        return [por, dam, sheed, jimbo, adam]
+    }
 }
