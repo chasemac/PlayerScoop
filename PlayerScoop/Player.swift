9cae8ea5da2f1d7430fae80ef9068ec2b42cdcc2
Chase McElroy <ichasem@gmail.com>
2016-06-23 10:03:51 -0700

    created Player class and split VC

---
+++ PlayerScoop/Player.swift
@@ -0,0 +1,20 @@
+//
+//  Player.swift
+//  PlayerScoop
+//
+//  Created by Chase McElroy on 6/23/16.
+//  Copyright © 2016 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class Player {
+    var name = ""
+    var age = 0
+    var college = ""
+    var height = ""
+    var image = UIImage()
+    var backgroundColor = UIColor()
+    var textColor1 = UIColor()
+    var textColor2 = UIColor()
+}
